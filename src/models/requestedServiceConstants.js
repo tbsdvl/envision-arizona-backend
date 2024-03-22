8dc5b08ac8d764300362caff1718264ab4d1f3f8
Triston Burns <tristonburns97@gmail.com>
2024-03-22 07:45:45 +0000

    TB-4 add community management service type

--- src/models/requestedServiceConstants.js
+++ src/models/requestedServiceConstants.js
@@ -1,5 +1,6 @@
 export const RequestedServiceConstants = {
     financialManagement: 'Financial Management',
-    gateServices: 'Gate Services',
+    gateManagement: 'Gate Management',
+    communityManagement: 'Community Management',
     other: 'other'
 }
